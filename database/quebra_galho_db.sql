3b5b61cc0565ffec01add43ca8d0fd35e3b289b2
carloshcp <carloshcp@gmail.com>
2015-09-22 00:50:40 -0300

    atualizacao tabela emprego. inseridas categorias de emprego e links de fotos

--- database/quebra_galho_db.sql
+++ database/quebra_galho_db.sql
@@ -4,7 +4,7 @@ USE `servicosonline`;
 --
 -- Host: 127.0.0.1    Database: servicosonline
 -- ------------------------------------------------------
--- Server version	5.6.25
+-- Server version	5.6.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -29,7 +29,7 @@ CREATE TABLE `emprego` (
   `emprego` varchar(255) DEFAULT NULL,
   `img_url` varchar(999) DEFAULT NULL,
   PRIMARY KEY (`id_emprego`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +38,7 @@ CREATE TABLE `emprego` (
 
 LOCK TABLES `emprego` WRITE;
 /*!40000 ALTER TABLE `emprego` DISABLE KEYS */;
-INSERT INTO `emprego` VALUES (1,'Pedreiro','img/pedrero.jpg'),(2,'Carpinteiro','img/carpinteiro.jpg'),(3,'Barbeiro','img/joao_barbeiro.jpg'),(4,'Gestor','img/carpinteiro.jpg');
+INSERT INTO `emprego` VALUES (1,'Adestrador','img/adestrador.jpg'),(2,'Administrador','img/administrador.jpg'),(3,'Advogado','img/advogado.jpg'),(4,'Alfaiate','img/alfaiate.jpg'),(5,'Arquiteto','img/arquiteto.jpg'),(6,'Artista circense','img/artistacircense.jpg'),(7,'Artista plástico','img/artistaplastico.jpg'),(8,'Ator','img/ator.jpg'),(9,'Barbeiro','img/barbeiro.jpg'),(10,'Barman','img/barman.jpg'),(11,'Cabeleireiro','img/cabeleireiro.jpg'),(12,'Cantor','img/cantor.jpg'),(13,'Carpinteiro','img/carpinteiro.jpg'),(14,'Confeiteiro','img/confeiteiro.jpg'),(15,'Contador','img/contador.jpg'),(16,'Cozinheiro','img/cozinheiro.jpg'),(17,'Costureiro','img/costureiro.jpg'),(18,'Dançarino','img/dancarino.jpg'),(19,'Dentista','img/dentista.jpg'),(20,'Depilador','img/depilador.jpg'),(21,'Designer','img/designer.jpg'),(22,'Despachante','img/despachante.jpg'),(23,'Diarista','img/diarista.jpg'),(24,'DJ','img/dj.jpg'),(25,'Educador físico','img/educadorfisico.jpg'),(26,'Eletricista','img/eletricista.jpg'),(27,'Encanador','img/encanador.jpg'),(28,'Enfermeiro','img/enfermeiro.jpg'),(29,'Engenheiro','img/engenheiro.jpg'),(30,'Estatístico','img/estatistico.jpg'),(31,'Fisioterapeuta','img/fisioterapeuta.jpg'),(32,'Fonoaudiólogo','img/fonoaudiologo.jpg'),(33,'Fotógrafo','img/fotografo.jpg'),(34,'Guia de turismo','img/guiadeturismo.jpg'),(35,'Jardineiro','img/jardineiro.jpg'),(36,'Lavador de veículos automotores','img/lavador.jpg'),(37,'Manicure','img/manicure.jpg'),(38,'Maquiador','img/maquiador.jpg'),(39,'Marceneiro','img/marceneiro.jpg'),(40,'Massagista','img/massagista.jpg'),(41,'Mecânico','img/mecanico.jpg'),(42,'Médico','img/medico.jpg'),(43,'Montador de móveis e equipamentos','img/montador.jpg'),(44,'Motoboy','img/motoboy.jpg'),(45,'Motorista','img/motorista.jpg'),(46,'Músico','img/musico.jpg'),(47,'Nutricionista','img/nutricionista.jpg'),(48,'Padeiro','img/padeiro.jpg'),(49,'Pedicure','img/pedicure.jpg'),(50,'Pedreiro','img/pedreiro.jpg'),(51,'Pintor','img/pintor.jpg'),(52,'Professor','img/professor.jpg'),(53,'Profissional de banho e tosa','img/profissionalbanhoetosa.jpg'),(54,'Profissional de manutenção de piscinas','img/profissionalpiscinas.jpg'),(55,'Programador','img/programador.jpg'),(56,'Psicólogo','img/psicologo.jpg'),(57,'Publicitário','img/publicitario.jpg'),(58,'Recepcionista','img/recepcionista.jpg'),(59,'Relações públicas','img/rp.jpg'),(60,'Relojoeiro','img/relojoeiro.jpg'),(61,'Revisor de textos','img/revisor.jpg'),(62,'Segurança','img/seguranca.jpg'),(63,'Serralheiro','img/serralheiro.jpg'),(64,'Técnico em iluminação','img/tecnicoiluminacao.jpg'),(65,'Técnico em informática','img/tecnicoinformatica.jpg'),(66,'Técnico em manutenção','img/tecnicomanutencao.jpg'),(67,'Técnico em som','img/tecnicosom.jpg'),(68,'Tradutor','img/tradutor.jpg'),(69,'Veterinário','img/veterinario.jpg'),(70,'Vidraceiro','img/vidraceiro.jpg');
 /*!40000 ALTER TABLE `emprego` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -95,7 +95,7 @@ CREATE TABLE `servico` (
   KEY `id_usuario` (`id_usuario`),
   CONSTRAINT `servico_ibfk_1` FOREIGN KEY (`id_emprego`) REFERENCES `emprego` (`id_emprego`),
   CONSTRAINT `servico_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -104,7 +104,7 @@ CREATE TABLE `servico` (
 
 LOCK TABLES `servico` WRITE;
 /*!40000 ALTER TABLE `servico` DISABLE KEYS */;
-INSERT INTO `servico` VALUES (2,'Ser rápido e veloz','x',0,'SP',NULL,11,1),(3,'Jogo 10','x',1,'RS',NULL,2,2),(4,'Trabalho com felicidade','x',0,'RJ',NULL,3,3),(5,'Administro bem','x',0,'ALL',NULL,11,4);
+INSERT INTO `servico` VALUES (2,'Ser rápido e veloz','x',0,'SP',NULL,11,1),(3,'Jogo 10','x',1,'RS',NULL,2,2),(4,'Trabalho com felicidade','x',0,'RJ',NULL,3,3),(5,'Administro bem','x',0,'ALL',NULL,11,4),(6,'bebida aa','asasa',0,'ALL',NULL,12,10);
 /*!40000 ALTER TABLE `servico` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -148,4 +148,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-09-18 13:57:07
+-- Dump completed on 2015-09-22  0:48:01
