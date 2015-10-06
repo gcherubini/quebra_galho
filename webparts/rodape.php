a3c005db021cc33e6c0e54126cc83c419b9b8126
System Administrator <root@Mateuss-MacBook-Pro.local>
2015-10-05 21:58:16 -0300

    rodape adicionado, e edit css

--- webparts/rodape.php
+++ webparts/rodape.php
@@ -1,3 +1,49 @@
-<p class="texto_rodape">
-...rodape...
-</p>
+<div class = "footer">
+	
+	<div class = "footer_text">
+
+		
+
+	
+	<div class = "footer_sobre">
+
+		<a href= "sobre.php"> Sobre Nós </a>
+
+	</div>
+
+	<div class  = "footer_politicas">
+
+	<a href = "politicas.php"> Termos de Uso | Política de Responsabilidade </a> 
+	
+		</div>
+
+			<div class = "footer_contato">
+
+			<a href = "contato.php"> Fale Conosco </a>
+
+			</div>
+
+				<div class = "footer_ajuda">
+
+
+		
+					<a href = "ajuda.php"> Central de Ajuda </a>
+
+				</div>
+	
+
+
+	
+	</div>
+
+	<div class = "footer_copyright">
+
+		© QuebraGalho 2015
+		
+
+	</div>
+
+	
+
+
+</div>
