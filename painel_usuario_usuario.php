43e1b2f2aa7b02bbe56eba6eccca2b09e3864302
carloshcp <carloshcp@gmail.com>
2015-10-14 23:32:05 -0300

    Painel do usuário V1 - Content e layout

--- painel_usuario_usuario.php
+++ painel_usuario_usuario.php
@@ -20,6 +20,23 @@ if(!isset($_SESSION['id_usuario'])){
 
  	});
 
+	$().ready(function() {
+
+
+	    $('#clicker').click(function() {
+	        $('.painel_usuario_main input').each(function() {
+	            if ($(this).attr('disabled')) {
+	                $(this).removeAttr('disabled');
+	            }
+	            else {
+	                $(this).attr({
+	                    'disabled': 'disabled'
+	                });
+	            }
+	        });
+	    });
+	});
+
  	</script>
   </head>
   <body>
@@ -40,9 +57,91 @@ if(!isset($_SESSION['id_usuario'])){
 
 				<h3> Painel do usuário </h3>
 				 
-				<p>
-				 	Olá <b> <?php echo $_SESSION["nome"]; ?> </b>, seja bem vindo ao seu painel de configurações 
-				</p>
+					<p>
+						Olá <b> <?php echo $_SESSION["nome"]; ?></b>, seja bem-vindo(a) ao seu painel de configurações.
+					</p>
+
+					<p>
+						Aqui você encontra um resumo sobre o seu perfil, onde pode modificar algumas informações pessoais e configurações da sua conta.
+					</p>
+
+				<h4> MEU PERFIL </h4>
+
+					<p>
+						Nome: <?php echo $_SESSION["nome"]; ?>
+					</p>
+
+					<p>
+						E-mail: <input type='text' disabled></input>
+					</p>
+
+					<p>
+						Senha: <input type='password' disabled></input>
+					</p>
+
+					<p>
+						Telefone: <input type='text' disabled></input>
+					</p>
+
+					<p>
+						Endereço: <input type='text' disabled></input>
+					</p>
+
+					<div id='clicker' class="btn btn-default">Editar</div>
+
+					<div class="btn btn-default">Salvar</div>
+
+				<h4> MINHAS AVALIAÇÕES </h4>
+
+					<p>
+						Você possui X avaliações.
+					</p>
+
+					<p>
+						Sua reputação atual é X.
+					</p>
+
+					<p>
+						Você possui X avaliações.
+					</p>
+
+					<p>
+						<a href="#">Ver minhas avaliações</a>
+					</p>
+
+				<h4> MEUS ANÚNCIOS </h4>
+
+					<p>
+						Você possui X anúncios cadastrados.
+					</p>
+
+					<p>
+						<a href="#">Ver meus anúncios</a> - <a href="#">Promova agora o seu anúncio!</a>
+					</p>
+
+				<h4> MINHAS NEGOCIAÇÕES </h4>
+
+					<p>
+						X usuários já solicitaram o seu contato para negociar.
+					</p>
+
+					<p>
+						<a href="#">Ver minhas negociações</a>
+					</p>
+
+				<h4> CONFIGURAÇÕES DA CONTA </h4>
+
+					<p>
+						Gostaria de receber notificações no meu email (s/n)
+					</p>
+
+					<p>
+						Gostaria de receber ofertas do quebra-galho no meu email (s/n)
+					</p>
+
+					<p>
+						<a href="#">Deletar conta</a>
+					</p>
 
 			</div>
 	<?php
