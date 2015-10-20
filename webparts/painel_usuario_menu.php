ac27607c387cfd564ea59a2435be2ae87fa1442b
carloshcp <carloshcp@gmail.com>
2015-10-20 15:21:55 -0200

    Inserida a aba das avaliações e alteração das descrições nas abas

--- webparts/painel_usuario_menu.php
+++ webparts/painel_usuario_menu.php
@@ -1,7 +1,8 @@
 <ul class="nav nav-tabs nav-justified painel_menu">
-  <li role="presentation" id="painel_menu_usuario"><a href="painel_usuario_usuario.php">Usuário</a></li>
-  <li role="presentation" id="painel_menu_servicos"><a href="painel_usuario_servicos.php">Serviços</a></li>
-  <li role="presentation" id="painel_menu_negociacoes"><a href="painel_usuario_negociacoes.php">Negociações</a></li>
+  <li role="presentation" id="painel_menu_usuario"><a href="painel_usuario_usuario.php"><span class="glyphicon glyphicon-lock"></span> Minha conta </a></li>
+  <li role="presentation" id="painel_menu_avaliacoes"><a href="#"><span class="glyphicon glyphicon-star"></span> Minhas avaliações </a></li>
+  <li role="presentation" id="painel_menu_servicos"><a href="painel_usuario_servicos.php"><span class="glyphicon glyphicon-comment"></span> Meus anúncios </a></li>
+  <li role="presentation" id="painel_menu_negociacoes"><a href="painel_usuario_negociacoes.php"><span class="glyphicon glyphicon-resize-horizontal"></span> Minhas negociações </a></li>
   <li role="presentation" id="painel_menu_notificacoes"><a href="painel_usuario_notificacoes.php">
   	Notificações 
   	<span class="glyphicon glyphicon-exclamation-sign notificacao-icon"> </span> </a>
