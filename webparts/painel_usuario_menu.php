f93ee9ff2817f17bbdfbea8d4ca7125b84ad55a3
carloshcp <carloshcp@gmail.com>
2015-10-20 18:09:20 -0200

    Ajustes de conteúdo e layout no menu do painel do usuário

--- webparts/painel_usuario_menu.php
+++ webparts/painel_usuario_menu.php
@@ -1,6 +1,6 @@
 <ul class="nav nav-tabs nav-justified painel_menu">
   <li role="presentation" id="painel_menu_usuario"><a href="painel_usuario_usuario.php"><span class="glyphicon glyphicon-lock"></span> Minha conta </a></li>
-  <li role="presentation" id="painel_menu_avaliacoes"><a href="#"><span class="glyphicon glyphicon-star"></span> Minhas avaliações </a></li>
+  <li role="presentation" id="painel_menu_avaliacoes"><a href="painel_usuario_avaliacoes.php"><span class="glyphicon glyphicon-star"></span> Minhas avaliações </a></li>
   <li role="presentation" id="painel_menu_servicos"><a href="painel_usuario_servicos.php"><span class="glyphicon glyphicon-comment"></span> Meus anúncios </a></li>
   <li role="presentation" id="painel_menu_negociacoes"><a href="painel_usuario_negociacoes.php"><span class="glyphicon glyphicon-resize-horizontal"></span> Minhas negociações </a></li>
   <li role="presentation" id="painel_menu_notificacoes"><a href="painel_usuario_notificacoes.php">
